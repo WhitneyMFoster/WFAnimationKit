b6436ffab8bff48d334fec1944f271d527ff1319
Whitney Foster <whitney@clubw.com>
2015-06-02 17:22:10 -0700

    Initial Commit

---
+++ WFAnimationKit/WFAnimationKit.h
@@ -0,0 +1,19 @@
+//
+//  WFAnimationKit.h
+//  WFAnimationKit
+//
+//  Created by Whitney Foster on 6/2/15.
+//  Copyright (c) 2015 WhitneyFoster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WFAnimationKit.
+FOUNDATION_EXPORT double WFAnimationKitVersionNumber;
+
+//! Project version string for WFAnimationKit.
+FOUNDATION_EXPORT const unsigned char WFAnimationKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WFAnimationKit/PublicHeader.h>
+
+
