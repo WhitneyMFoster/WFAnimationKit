ac2811eea4c5490fd6f20d2d159cb1a667dca0cd
Whitney Foster <whitney@clubw.com>
2015-06-03 12:15:08 -0700

    first commit

---
+++ WFAnimationKit/WFAnimationObject.swift
@@ -0,0 +1,9 @@
+//
+//  WFAnimationObject.swift
+//  WFAnimationKit
+//
+//  Created by Whitney Foster on 6/2/15.
+//  Copyright (c) 2015 WhitneyFoster. All rights reserved.
+//
+
+import Foundation
